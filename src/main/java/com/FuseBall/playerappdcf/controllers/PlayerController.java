93d9c6ebbf8974ef49af799e85d5b25f79727887
Donald Farrar <donfar09@gmail.com>
2022-03-24 20:01:36 -0700

    fixing stuff that was in the wrong area

---
+++ src/main/java/com/FuseBall/playerappdcf/controllers/PlayerController.java
@@ -0,0 +1,27 @@
+package com.FuseBall.playerappdcf.controllers;
+
+
+import com.FuseBall.playerappdcf.entity.Player;
+import com.FuseBall.playerappdcf.repository.PlayerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    @Autowired
+    private PlayerRepo playerRepo;
+
+    @PostMapping
+    public Player addPlayer(@RequestBody Player player){
+        return playerRepo.save(player);
+    }
+
+    @GetMapping("/{jerseyNum}")
+    public Player getPlayer(@PathVariable("jerseyNum") Integer jerseyNum){
+        return playerRepo.findById(jerseyNum).get();
+    }
+}
---
+++ src/test/java/com/FuseBall/playerappdcf/PlayerAppDcfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FuseBall.playerappdcf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerAppDcfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
