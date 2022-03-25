ac485c51a3679638cb7b10d18bb0f72795647c14
Donald Farrar <donfar09@gmail.com>
2022-03-24 19:56:19 -0700

    fixed the connection issue

--- player-app-dcf/src/main/java/com/FuseBall/playerappdcf/controllers/PlayerController.java
+++ player-app-dcf/src/main/java/com/FuseBall/playerappdcf/controllers/PlayerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("players")
+@RequestMapping("/player")
 public class PlayerController {
 
     @Autowired
